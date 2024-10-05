1da43ac9fb1b3074a6d26dc745a699e02ae2b7df
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-05 07:56:53 +0000

    learned dataTypes

--- _basics_of_javascript/02_dataTypes.js
+++ _basics_of_javascript/02_dataTypes.js
@@ -1,3 +1,22 @@
 "use strict"; // treat all JS Code as newer version
 
 // alert("Allow Cookies") // we are using nodejs, not browser
+
+// remember : code readability should be good/high enough.
+
+let name = "jeetuSingh" // string
+let age = 20 // number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+// number range --> 2 to power 53
+// bigint --> for larger integer value
+// string --> "String comes here"
+// boolean --> true/false
+// null --> standalone value
+// undefined --> value is not defined yet
+// symbol --> unique always
+
+// object
+
+console.log(typeof age)
