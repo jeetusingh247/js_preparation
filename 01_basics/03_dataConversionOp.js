811fcb08b801351805e92bfa715ba7c47edfb679
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-05 09:38:38 +0000

    added dataconversion

---
+++ 01_basics/03_dataConversionOp.js
@@ -0,0 +1,24 @@
+/*
+let score = "jsingh74"
+let score2 = null
+let score3 = undefined
+let score4 = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+// Type Conversion(string --> number)
+let valInNum1 = Number(score);
+let valInNum2 = Number(score2);
+let valInNum3 = Number(score3);
+let valInNum4 = Number(score4);
+console.log("the value of score is: ",valInNum1); // Not a number
+console.log("the value of score2 is: ",valInNum2); // 0
+console.log("the value of score3 is: ",valInNum3); // Not a number
+console.log("the value of score4 is: ",valInNum4); // 1
+*/
+
+let isLoggedIn = 1;
+
+let boolisLoggedIn = Boolean(isLoggedIn)
+console.log(boolisLoggedIn) // true
