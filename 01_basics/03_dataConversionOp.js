4f4ee4fc31440a8d4e4bde72781b1e7defe9a208
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-06 13:10:18 +0000

    comparison operation

--- 01_basics/03_dataConversionOp.js
+++ 01_basics/03_dataConversionOp.js
@@ -18,7 +18,7 @@ console.log("the value of score3 is: ",valInNum3); // Not a number
 console.log("the value of score4 is: ",valInNum4); // 1
 */
 
-let isLoggedIn = 1;
+/*let isLoggedIn = 1;
 
 let boolisLoggedIn = Boolean(isLoggedIn)
 console.log(boolisLoggedIn) // true => 1 and false => 0
@@ -27,4 +27,46 @@ console.log(boolisLoggedIn) // true => 1 and false => 0
 let someNum = 33;
 let strNum = String(someNum);
 console.log("string number result : ", strNum);
-console.log("string number type : ", typeof strNum);
+console.log("string number type : ", typeof strNum);*/
+
+// ********************* Operations *********************//
+
+/*let val = 34;
+let negval = -val;
+console.log(negval);
+
+// --> Arithmetic Operations
+
+// console.log(2+2); // -->add
+// console.log(2-2); // --> subtract
+// console.log(2*2); // --> multiply
+// console.log(2/2); // --> divide
+// console.log(2%2); // --> modulus
+// console.log(2**2); // --> power
+
+let str1 = "Jai Shree"
+let str2 = " Ram"
+console.log("Bolo Saare",(str1 + str2))*/
+
+// --> Differ in outputs due to operator precedence
+// console.log("1" + 2) 
+// console.log("1" + "2")
+// console.log("1" + 2 + 2) // --> 122
+// console.log(1 + 2 + "2") // --> 32
+// console.log((3+2) * 5 + (9-4)); // --> 30
+
+// console.log(+true)
+// console.log(+"")
+
+// let counter = 107;
+// counter++; --> post increment(value increase after use)
+// ++counter; // --> pre increment(value increase before use)
+// console.log(counter, " baar");
+// study from mdn is better
+
+
+
+
+
+
+
