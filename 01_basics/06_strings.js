54f677032d2e46e6cca39a32985265dd0b8d51c4
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-11 01:53:37 +0000

    string methods

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,38 @@
+/*
+Strings are useful for holding data that can be represented in text form. Some of the most-used operations on strings are to check their length, to build and concatenate them using the + and += string operators, checking for the existence or location of substrings with the indexOf() method, or extracting substrings with the substring() method.
+*/
+const name = "jeetu"
+const repoCount = 34
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   removalSpace    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://prototype.com/theme_prototype"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
