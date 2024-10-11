6cb83b07bb18c401222298fedad75fa3c6e914da
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-11 12:29:32 +0000

    updated 07_num

--- 01_basics/07_num_math.js
+++ 01_basics/07_num_math.js
@@ -13,6 +13,32 @@ console.log(num2.toPrecision(3))// returns precise value upto 3 digit from left 
 const hundreds = 100000
 console.log(hundreds.toLocaleString('en-IN')); // return number as per indian standards
 
+// ************************ Maths ******************************
+/*
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.round(4.6));// 5
+console.log(Math.floor(4.9));// 4
+console.log(Math.sqrt(8).toPrecision(2)); // we can also do this way
+// console.log(Math.sqrt(min(3, 4, 5))); cannot do this way
+*/
+
+// console.log(Math.random()); // returns value is inBetween 0-1
+// console.log((Math.random()*10) + 1); 
+
+const min = 30
+const max = 80
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // returns floor values
+
+
+
+
+
+
+
+
 
 
 
