1c2734f2e545299af69d6ca84a5a318dd327f6ad
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-13 14:50:51 +0000

    array methods updated

---
+++ 02_basics/02_array.js
@@ -0,0 +1,27 @@
+const akhand_powers =["chandragupt", "Buddha", "Arjun", "Ram", "HanumanJi"]
+
+const rishi_muni = ["ashtavakra", "aaryabahtta", "dronacharya", "rishiVashisth"]
+
+// akhand_powers.push(rishi_muni)
+
+// console.log(akhand_powers); // --> merged arrays act as single element
+// console.log(akhand_powers[5]); // --> solution
+
+// const all_powers = akhand_powers.concat(rishi_muni)
+// console.log(all_powers) // --> both array are merged by elements
+
+// const all_newPowers = [...akhand_powers, ...rishi_muni] // spread operator
+// console.log(all_newPowers);
+
+// const ano_arr = [1, 2, 3, [8, [4, 5, 6], 7, [4, 5]]]
+// const bc_arr = ano_arr.flat(Infinity)
+// console.log(ano_arr);
+// console.log(bc_arr) // --> values are auto spreadOut
+
+
+
+
+
+
+
+
