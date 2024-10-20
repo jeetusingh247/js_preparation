efba117b41e7008b8eda843027675b6ecc222d06
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-20 07:11:17 +0000

    added about while, do-while

---
+++ 05_loops_iterations/02_iterations.js
@@ -0,0 +1,24 @@
+// Basic while loop
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+// Basic do-while loop(entry control loop)
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+// loops are majourly used to fetch data from DB stored in form of array
