985b36f6739abb74c1f43b8d6fe3964a56cf82a9
Jeetu Singh <143892753+jeetusingh247@users.noreply.github.com>
2024-10-21 05:10:17 +0000

    modified iteration08

--- 05_loops_iterations/08_iterations.js
+++ 05_loops_iterations/08_iterations.js
@@ -9,3 +9,27 @@ const Nums = [1,2,3,4]
 
 const myTotal = Nums.reduce( (acc, curr) => acc + curr, 0) // using arrow func
 console.log(myTotal); // Ans : 6
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
